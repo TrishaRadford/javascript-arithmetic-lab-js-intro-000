b4722f18908c79ffa64a19d78a8445b142355688
convivial-reducer-7231 <tradford10@gmail.com>
2020-10-05 16:11:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,8 +11,14 @@ function subtract(){
   return result;
 }
 function multiply(){
-
+  var a = 1;
+  var b = 2;
+  var result = a*b;
+  return result;
 }
 function divide(){
-
+  var a = 1;
+  var b = 2;
+  var result = a/b;
+  return result;
 }
