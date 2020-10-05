fe4d9796b95e28134bbe440f28a7afd168f3f2da
convivial-reducer-7231 <tradford10@gmail.com>
2020-10-05 16:50:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ function divide(a, b){
   var result = a / b;
   return result;
 }
-var n = 5;
-function increments() {
-  n++;
-  return n;
+function increments(n) {
+  var n = 5;
+  var result = n++;
+  return result;
 }
